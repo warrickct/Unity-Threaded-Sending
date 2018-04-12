6db477dac7226e4d84bdefea72cb5f652ff8c570
Warrick Corfe-Tan <wcor690@UoA.auckland.ac.nz>
2018-04-12 12:42:45 +1200

    Added test file for sending and created tcp client and listener threaded functions

--- Assets/Scripts/ThreadNet.cs
+++ Assets/Scripts/ThreadNet.cs
@@ -5,10 +5,19 @@ using System.Threading;
 using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
 public class ThreadNet : MonoBehaviour {
 
     public GameObject model;
+
+    //Debugging 
+    string debugData = null;
+
+    //Debugging 
+    bool connected = false;
 
 	// Use this for initialization
 	void Start () {
@@ -22,8 +31,18 @@ public class ThreadNet : MonoBehaviour {
         Thread newThread = new Thread(() => SerializeModel(vertices, uv, triangles));
         newThread.IsBackground = true;
         newThread.Start();
+
+        Thread listenerThread = new Thread(Listener);
+        listenerThread.IsBackground = true;
+        listenerThread.Start();
     }
 
+    /// <summary>
+    /// Extracts model vert, uv, triangle. Converts them into float arrays, constructs into serializable class, serializes into byte array.
+    /// </summary>
+    /// <param name="verts"></param>
+    /// <param name="uvs"></param>
+    /// <param name="triangles"></param>
     void SerializeModel(Vector3[] verts, Vector2[] uvs, int[] triangles)
     {
         List<float> vertFloats = new List<float>();
@@ -51,6 +70,83 @@ public class ThreadNet : MonoBehaviour {
         MemoryStream ms = new MemoryStream();
         BinaryFormatter bf = new BinaryFormatter();
         bf.Serialize(ms, wd2);
+        byte[] data = ms.ToArray();
+
+        Debug.Log("hello");
+        Thread senderThread = new Thread(() => SenderThreaded(data));
+        senderThread.IsBackground = true;
+        senderThread.Start();
+
+        Debug.Log("sender thread started");
+    }
+
+    /// <summary>
+    /// Sends byte array using sockets
+    /// </summary>
+    /// <param name="data"></param>
+    void SenderThreaded(byte[] data)
+    {
+        IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12001);
+
+        Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        Debug.Log("client set up");
+
+        client.Connect(ipEndPoint);
+        Debug.Log("connected");
+
+        /*
+        client.SendFile("test.txt");
+        Debug.Log("Sent");
+        client.Shutdown(SocketShutdown.Send);
+        */
+
+        string message = "sending this message";
+        byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+        client.Send(messageBytes);
+    }
+
+    void Listener()
+    {
+        IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12001);
+        TcpListener tcpListener = new TcpListener(ipEndPoint);
+
+        tcpListener.Start();
+
+        byte[] bytes = new byte[256];
+        String data = null;
+
+        while (true)
+        {
+            TcpClient client = tcpListener.AcceptTcpClient();
+
+            //Debugging 
+            connected = true;
+
+            data = null;
+
+            NetworkStream netStream = client.GetStream();
+
+            int i;
+
+            while ((i = netStream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                data = Encoding.ASCII.GetString(bytes, 0, i);
+                data.ToUpper();
+                debugData = "received " + data;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        //Debugging 
+        if (debugData != null)
+        {
+            Debug.Log(debugData);
+        }
+
+        //Debugging 
+        Debug.Log(connected);
     }
 }
 
